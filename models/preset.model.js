6289f35602727504ab2f6df6cac60515c136ee85
billymoroney <YOUR-EMAIL-ADDRESS>
2021-02-13 14:13:01 -0600

    changed model for testing

--- models/preset.model.js
+++ models/preset.model.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const Preset = mongoose.model(
     'Preset',
     new mongoose.Schema({
-        name: String,
-        options: []
+        name: String
     })
 )
 
