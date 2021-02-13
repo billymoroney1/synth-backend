218e2f6843d5a6bb86f8d2887672622750f697a3
billymoroney <YOUR-EMAIL-ADDRESS>
2021-02-13 13:59:23 -0600

    updated preset middleware

--- routes/presets.routes.js
+++ routes/presets.routes.js
@@ -2,16 +2,17 @@ const controller = require('../controllers/presets.controller')
 
 module.exports = function(app) {
     app.use((req, res, next) => {
-        //set header and allow use of access token
+        //set header and allow use of x-access token
         res.header(
             "Access-Control-Allow-Headers",
             "x-access-token, Origin, Content-type, Accept"
         )
+        res.header("Access-Control-Allow-Origin", '*')
         next()
     })
 
     // //create new preset
-    // app.post("/api/presets/preset", controller.createPreset)
+    app.post("/api/presets/preset", controller.createPreset)
     // //update preset
     // app.put("/api/presets/preset", controller.editPreset)
     // //delete preset
