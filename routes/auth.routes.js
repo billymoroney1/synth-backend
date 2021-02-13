6465353e63e1a6bfc7f176cf21c108a213a2f93f
billymoroney <YOUR-EMAIL-ADDRESS>
2021-02-13 12:15:29 -0600

    commented out custom middleware for test

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,15 +2,15 @@ const { verifyRegister } = require('../middlewares')
 const controller = require('../controllers/auth.controller')
 
 module.exports = function(app) {
-    app.use((req, res, next) => {
-        //set header and allow use of x-access token
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Access-Control-Allow-Origin",
-            "x-access-token, Origin, Content-type, Accept"
-        )
-        next()
-    })
+    // app.use((req, res, next) => {
+    //     //set header and allow use of x-access token
+    //     res.header(
+    //         "Access-Control-Allow-Headers",
+    //         "Access-Control-Allow-Origin",
+    //         "x-access-token, Origin, Content-type, Accept"
+    //     )
+    //     next()
+    // })
     //set up signup route and pass middleware to check username, email, and roles
     app.post("/api/auth/register", verifyRegister.checkDuplicateUsernameOrEmail, controller.register)
     //handle sign in
